d75bea7376b083dbddca4176b0f8f8c6c675d366
Eddie Abou-Jaoude <eddie@jaoudestudios.com>
2017-06-16 07:21:42 +0100

    Filter communities by tag

--- src/app/communities/communities.component.ts
+++ src/app/communities/communities.component.ts
@@ -11,6 +11,8 @@ import { CommunityService } from './community.service';
 export class CommunitiesComponent implements OnInit {
   communities: Array<Community>;
   total: Number;
+  displaying: Number;
+  tags: Array<string> = [];
 
   constructor(private communityService: CommunityService) { }
 
@@ -18,12 +20,22 @@ export class CommunitiesComponent implements OnInit {
     this.getCommunities();
   }
 
-  getCommunities(): void {
+  getCommunities(filter?: string): void {
     this.communityService
       .getCommunities()
       .then(communities => {
-        this.communities = communities;
+
+        if (filter) {
+          this.communities = communities.filter((community) => community.tags.includes(filter));
+        } else {
+          this.communities = communities;
+        }
+
         this.total = communities.length;
+        this.displaying = this.communities.length;
+        communities
+          .map((community) => community.tags
+            .filter((tag) => !this.tags.includes(tag) ? this.tags.push(tag) : null));
       });
   }
 }
