e7da3b090d8cfe067d2f593fb4190cb76c93f13e
Eddie Abou-Jaoude <eddie@jaoudestudios.com>
2017-06-16 19:37:32 +0100

    Removed unused import in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { CommunityService } from './communities/community.service';
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
